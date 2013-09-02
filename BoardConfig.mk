3a88ac9e6eb7bad74e6d4ca12ddf068394701f49
madmack <alali48@gmail.com>
2013-09-02 11:32:40 -0400

    cm-10.2 upbringing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@
 #
 
 #BOARD_KERNEL_CMDLINE := console=ttySHL0,115200,n8 androidboot.hardware=gkatt lpj=67677
-BOARD_KERNEL_CMDLINE := vmalloc=600M console=ttyHSL0,115200,n8 lpj=67677 user_debug=31 msm_rtb.filter=0x0 ehci-hcd.park=3 coresight-etm.boot_enable=0 androidboot.hardware=geefhd_att_us uart_console=disable lge.rev=rev_10 lcd_maker_id=primary lge.hreset=off, lge.reset=rst_etc gpt=enable logoentry=0x77665501+0+0 lge.batt_info=ds2704_l lge.crash=0x0 lge.crash_enable=0x0
+BOARD_KERNEL_CMDLINE := vmalloc=600M console=null lpj=67677 user_debug=31 msm_rtb.filter=0x0 ehci-hcd.park=3 coresight-etm.boot_enable=0 androidboot.hardware=geefhd_att_us
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
